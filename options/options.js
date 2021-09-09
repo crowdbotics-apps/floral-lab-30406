d25a8025771cd88251e54efb69ba28b528bbdd1f
Crowdbotics <team@crowdbotics.com>
2021-09-09 02:28:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "floral_lab_30406Identifier",
+  url: "https://floral_lab_30406Identifier.botics.co",
+  api: "https://floral_lab_30406Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
